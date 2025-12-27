7603c385b98b9851fa01d95ea9757bb2a403ca0a
Anton James Rayos <antonjamesr7@gmail.com>
2025-12-27 13:25:37 +0800

    first commit

---
+++ routes/taskRoutes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const Task = require("../models/Task");
+
+// CREATE
+router.post("/", async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// READ ALL
+router.get("/", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+// UPDATE
+router.put("/:id", async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(task);
+});
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: "Task deleted" });
+});
+
+module.exports = router;
